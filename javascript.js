3765875584dc99db1e144a154d976561cd8ec8fd
aspen <jgromadzinska9@gmail.com>
2023-04-06 20:49:49 +0100

    Add operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+let firstNumber = 4;
+
+let secondNumber = 5;
+
+let operator = "+";
+
 const add = function(a,b) {
     return result = a + b;
 };
@@ -13,3 +19,25 @@ const multiply = function (a,b) {
 const divide = function (a,b) {
     return result = a / b;
 };
+
+//console.log(divide(firstNumber,secondNumber))
+
+const operate = function (operator,firstNumber,secondNumber) {
+    let result;
+    if (operator === "+") {
+        result = add(firstNumber,secondNumber);
+    } else if (operator === "-") {
+        result = subtract(firstNumber,secondNumber);
+    } else if (operator === "*") {
+        result = multiply(firstNumber,secondNumber);
+    } else if (operator === "/" && (firstNumber === 0 || secondNumber === 0)) {
+        result = "Nope.";
+    } else {
+        result = divide(firstNumber,secondNumber);
+    }
+
+    return result;
+
+};
+
+//console.log(operate("*",2,4));
